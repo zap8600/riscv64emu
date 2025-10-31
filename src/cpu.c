72e8d997848bad2f7b657385b429a9ee86a7cc7f
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-10-31 09:52:53 -0500

    Add cpu_init
    Added initialization function for cpu_t struct.

--- src/cpu.c
+++ src/cpu.c
@@ -1,1 +1,9 @@
+#include <cpu.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Initialize a `cpu_t` struct. Caller must free `dram` themselves.
+void cpu_init(cpu_t* cpu, size_t dram_size) {
+    memset(cpu->regs, 0, 32 * sizeof(uint64_t));
+    cpu->dram = (uint8_t*)malloc(dram_size);
+}
